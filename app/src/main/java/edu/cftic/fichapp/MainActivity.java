771385ddfcbac064cdf751a1f2d2e5e480cc068d
aliciaMorenoOliva <lamorenna2@hotmail.es>
2019-06-12 13:45:44 +0200

    seguimos con el json creamos holder y adapter

--- app/src/main/java/edu/cftic/fichapp/MainActivity.java
+++ app/src/main/java/edu/cftic/fichapp/MainActivity.java
@@ -4,40 +4,45 @@ import android.content.res.AssetFileDescriptor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recView;
+
+    private ArrayList<Programador> datos;
+
+    private AdapterCreditos adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Get the uri of the image from raw folder
-        Uri uri = Uri.parse(
-                "android.resource://"
-                        + getPackageName()
-                        + "/"
-                        + R.raw.programadores
-        );
 
-        // Display the raw resource uri in text view
-        Log.d( "MIAPP","URI of the image : \n"+uri.toString());
-        try {
-            InputStream ins = getResources().openRawResource(R.raw.programadores);
+        recView = (RecyclerView) findViewById(R.id.RecView);
 
-            InputStreamReader br = new InputStreamReader(ins, "UTF-8");
-            Gson gson = new Gson();
-            //gson.fromJson()
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        adaptador = new AdapterCreditos(datos);
+
+        recView.setAdapter(adaptador);
+
+        recView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
+
+        recView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+
     }
+
 }
