978474822c562ff5473c8999670026d3102fad43
aliciaMorenoOliva <lamorenna2@hotmail.es>
2019-06-17 12:15:59 +0200

    retoques y comentarios

--- app/src/main/java/edu/cftic/fichapp/Creditos.java
+++ app/src/main/java/edu/cftic/fichapp/Creditos.java
@@ -48,11 +48,13 @@ public class Creditos extends AppCompatActivity {
         // Display the raw resource uri in text view
         Log.d("MIAPP", "URI of the image : \n" + uri.toString());
         try {
+
             InputStream ins = getResources().openRawResource(R.raw.programadores);
 
             InputStreamReader br = new InputStreamReader(ins, "UTF-8");
             Gson gson = new Gson();
 
+            //le metemos al json los datos del array
             final Type tipoEnvoltorioEmpleado = new TypeToken<ArrayList<Programador>>() {
             }.getType();
             datos = gson.fromJson(br, tipoEnvoltorioEmpleado);
@@ -62,6 +64,7 @@ public class Creditos extends AppCompatActivity {
 
             Collections.shuffle(datos); //hace que el orden en el que salen las filas sea aleatorio cada vez que carga la aplicacion
 
+            //hacemos la referencia a AdapterCreditos
             adaptador = new AdapterCreditos(datos, this);
 
             recView.setAdapter(adaptador);
@@ -72,13 +75,20 @@ public class Creditos extends AppCompatActivity {
         } catch (Exception e) {
             Log.e( this.getClass().getCanonicalName(), "Error al procesar los datos de los creditos.",e);
         }
-        //metemos la imagen
+        //metemos la imagen de cftic
         ImageView cas= (ImageView) findViewById(R.id.imageViewLogoCas); //txt is object of TextView
         cas.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW);
                 browserIntent.setData(Uri.parse("https://www.cas-training.com/ "));
                 startActivity(browserIntent);
+            }
+        });
+        //metemos el huevo de pascua
+        cas.setOnLongClickListener (new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return  true;
             }
         });
         //metemos el texto con el enlace a la pagina cftic
@@ -104,11 +114,12 @@ public class Creditos extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+//creamos el click del onClick del boton github
     public void clickGithub(View view) {
         Uri webpage = Uri.parse((String) view.getTag());
         OpenWebPage(webpage);
     }
-
+//creamos el metodo para abrir el link del github, linkedin o email
     private void OpenWebPage(Uri webpage) {
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         String pregunta = "Con que app quieres continuar";
@@ -118,6 +129,7 @@ public class Creditos extends AppCompatActivity {
         }
     }
 
+    //creamos el click del onClick del boton linkedin
     public void clickLinkedin (View view){
         if( view.getVisibility() == View.VISIBLE) {
             Uri webpage = Uri.parse((String) view.getTag());
